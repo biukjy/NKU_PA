f2cb287ddc0eda9c17a5912fa0a416bf48f24e58
tracer-ics2017 <tracer@njuics.org>
2023-04-10 21:55:16 +0800

    >  compile 161220000 kjy Linux kjy-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  21:55:16 up  4:46,  1 user,  load average: 0.03, 0.06, 0.05 2ab952a27060355abb8d3902e8cc99bcaf0ecb48

--- ics2017/nemu/src/cpu/exec/all-instr.h
+++ ics2017/nemu/src/cpu/exec/all-instr.h
@@ -1,16 +1,38 @@
 #include "cpu/exec.h"
 
-make_EHelper(mov);
 
+//prefix.c
 make_EHelper(operand_size);
 
+
+//special.c
 make_EHelper(inv);
 make_EHelper(nemu_trap);
 
 
+//system.c
+
+
+
+//control.c
 make_EHelper(call);
+make_EHelper(ret);
+
+
+
+//arith.c
 make_EHelper(sub);
+
+
+//logic.c
 make_EHelper(xor);
+
+
+//data-mov.c
+make_EHelper(mov);
 make_EHelper(push);
 make_EHelper(pop);
-make_EHelper(ret);
+
+
+
+
