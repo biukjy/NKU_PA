a4fa37651a525d141d15985a677b4b5f72b21d84
tracer-ics2017 <tracer@njuics.org>
2023-04-12 09:23:36 +0800

    >  compile 161220000 kjy Linux kjy-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:23:36 up 36 min,  1 user,  load average: 0.08, 0.04, 0.01 8ac7d1c9bfdebbb66060adab87b8adef4cee83e

--- ics2017/nemu/src/monitor/diff-test/diff-test.c
+++ ics2017/nemu/src/monitor/diff-test/diff-test.c
@@ -149,7 +149,20 @@ void difftest_step(uint32_t eip) {
 
   // TODO: Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
-  TODO();
+  if(cpu.eip!=r.eip)
+  {
+      printf("eip diff:NEMU->0x%08x,QEMU->0x%08x\n",cpu.eip,r.eip);
+      diff=true;
+  }
+  for(int i=0;i<8;i++)
+  {
+      if(reg_l(i)!=r.array[i])
+      {
+	  printf("different in reg %s:when nemu.eip=0x%08x\n",regsl[i],reg_l(i));
+	  diff=true;
+      }
+
+  }
 
   if (diff) {
     nemu_state = NEMU_END;
