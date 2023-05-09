f091993766b5b20aa03a8d95f31a776e8715807b
tracer-ics2017 <tracer@njuics.org>
2023-05-09 12:42:14 +0800

    >  run 161220000 kjy Linux kjy-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:42:14 up  1:37,  1 user,  load average: 0.43, 0.25, 0.13 d01188cd928490803acdfd181f9b8ece40950323

--- ics2017/nexus-am/am/arch/x86-nemu/include/arch.h
+++ ics2017/nexus-am/am/arch/x86-nemu/include/arch.h
@@ -9,7 +9,7 @@
 struct _RegSet {
  //uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
  //int       irq;
-  uintptr_t edi,esi,ebp,esp,ecx,eax;
+  uintptr_t edi,esi,ebp,esp,ebx,edx,ecx,eax;
   int irq;
   uintptr_t error_code,eip,cs,eflags;
 };
