0b93c53f4061b84b77bd1c1dd2ecc6c29d360014
tracer-ics2017 <tracer@njuics.org>
2023-05-12 22:27:48 +0800

    >  run 161220000 kjy Linux kjy-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:27:48 up  2:46,  1 user,  load average: 0.31, 0.29, 0.19 b7d68e9c77b97a372a7465d2f7c9864c411b31df

--- ics2017/nanos-lite/src/syscall.c
+++ ics2017/nanos-lite/src/syscall.c
@@ -47,6 +47,7 @@ _RegSet* do_syscall(_RegSet *r) {
 	break;
     case SYS_write:
         SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);
+        break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
