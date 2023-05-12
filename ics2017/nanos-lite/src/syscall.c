47acb50e91cce5daadc16d9c750471e835ecb2eb
tracer-ics2017 <tracer@njuics.org>
2023-05-12 22:17:00 +0800

    >  run 161220000 kjy Linux kjy-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:17:00 up  2:35,  1 user,  load average: 0.13, 0.11, 0.07 be6893e983276a80b2d520b52082a66ed9eb8c05

--- ics2017/nanos-lite/src/syscall.c
+++ ics2017/nanos-lite/src/syscall.c
@@ -11,13 +11,32 @@ void sys_exit(int code)
     _halt(code);
 }
 
+uintptr_t sys_write(int fd,void *buf,size_t len)
+{
+    if(fd==1||fd==2)
+    {
+	char a;
+	for(int i=0;i<len;i++)
+	{
+	    memcpy(&a,(void*)buf+i,1);
+	    _putc(a);
+	}
+	return len;
+    }
+    else // when fd=3?????
+    {
+	panic("Unhandled fd=%d in sys_write()",fd);
+    }
+    return -1;
+}
+
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
-  a[3] = SYSCALL_ARG3(r);
-  a[4] = SYSCALL_ARG4(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
     case SYS_none:
@@ -26,6 +45,8 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit:
         sys_exit(a[1]);
 	break;
+    case SYS_write:
+        SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
