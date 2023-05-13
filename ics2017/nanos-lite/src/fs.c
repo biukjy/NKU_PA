22fbffcb59d5781c6b83fcd73dff3081cd6881d3
tracer-ics2017 <tracer@njuics.org>
2023-05-13 19:58:53 +0800

    >  run 161220000 kjy Linux kjy-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:58:53 up  5:45,  1 user,  load average: 0.23, 0.07, 0.02 f3dab2fb7168d87c7e6d1f88ac62f2b842032d93

--- ics2017/nanos-lite/src/fs.c
+++ ics2017/nanos-lite/src/fs.c
@@ -1,9 +1,17 @@
 #include "fs.h"
 
+extern void ramdisk_read(void *buf,off_t offset,size_t len);
+extern void ramdisk_write(const void *buf,off_t offset,size_t len);
+extern void dispinfo_read(void *buf,off_t offset,size_t len);
+extern void fb_write(const void *buf,off_t offset,size_t len);
+extern size_t events_read(void *buf,size_t len);
+
+
 typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -23,4 +31,77 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+size_t fs_filesz(int fd)
+{
+    return file_table[fd].size;
+}
+
+off_t disk_offset(int fd)
+{
+    return file_table[fd].disk_offset;
+}
+
+off_t get_open_offset(int fd)
+{
+    return file_table[fd].open_offset;
+}
+
+void set_open_offset(int fd,off_t n)
+{
+    if(n>file_table[fd].size)
+    {
+        n=file_table[fd].size;
+    }
+    file_table[fd].open_offset=n;
+}
+
+int fs_open(const char *pathname,int flags,int mode)
+{
+    for(int i=0;i<NR_FILES;i++)
+    {
+	if(strcmp(file_table[i].name,pathname)==0)
+	{
+	    file_table[i].open_offset=0;
+	    return i;
+	}
+    }
+    panic("this filename not exists!");
+    return -1;
+}
+
+ssize_t fs_read(int fd,void *buf,size_t len)
+{
+    assert(fd>=0&&fd<NR_FILES);
+    if(fd<3||fd==FD_FB)
+    {
+	return 0;
+    }
+    if(fd==FD_EVENTS)
+    {
+	return events_read(buf,len);
+    }
+
+    ssize_t n=fs_filesz(fd)-get_open_offset(fd);
+    if(n>len){ n=len; }
+
+    if(fd==FD_DISPINFO)
+    {
+	dispinfo_read(buf,get_open_offset(fd),n);
+    }
+    else
+    {
+	ramdisk_read(buf,disk_offset(fd)+get_open_offset(fd),n);
+    }
+
+    set_open_offset(fd,get_open_offset(fd)+n);
+    return n;
+
+
+}
+
+int fs_close(int fd)
+{
+    return 0;
 }
--- ics2017/nanos-lite/src/loader.c
+++ ics2017/nanos-lite/src/loader.c
@@ -1,13 +1,20 @@
 #include "common.h"
-
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
+  /*
   extern size_t get_ramdisk_size();
   extern void ramdisk_read(void*,off_t,size_t);
   size_t len=get_ramdisk_size();
   ramdisk_read(DEFAULT_ENTRY,0,len);
+  */
+
+  int fd=fs_open(filename,0,0);
+  fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
+  fs_close(fd);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
 
