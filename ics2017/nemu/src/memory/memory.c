1313ea7367add5b4ab9f14ff14eb25cc7358826d
tracer-ics2017 <tracer@njuics.org>
2023-05-27 10:07:37 +0800

    >  compile 161220000 kjy Linux kjy-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:07:37 up  5:47,  1 user,  load average: 0.10, 0.08, 0.02 d32ef6ecf14bbdd469b1aaeffd8158ca29bcb063

--- ics2017/nemu/src/memory/memory.c
+++ ics2017/nemu/src/memory/memory.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 #include "device/mmio.h"
+#include "memory/mmu.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -8,7 +9,40 @@
     guest_to_host(addr); \
     })
 
+
+#define PTE_ADDR(pte) ((uint32_t)(pte)& ~0xfff)
+#define PDX(va) (((uint32_t)(va)>>22)&0x3ff)
+#define PTX(va) (((uint32_t)(va)>>12)&0x3ff)
+#define OFF(va) ((uint32_t)(va)&0xfff)
+
+
 uint8_t pmem[PMEM_SIZE];
+
+
+paddr_t page_translate(vaddr_t addr,bool iswrite)
+{
+    CR0 cr0=(CR0)cpu.CR0;
+    if(cr0.paging && cr0.protect_enable)
+    {
+	CR3 cr3=(CR3)cpu.CR3;
+	PDE* pgdirs=(PDE*)PTE_ADDR(cr3.val);
+	PDE pde=(PDE)paddr_read((uint32_t)(pgdirs+PDX(addr)),4);
+	Assert(pde.present,"addr=0x%x",addr);
+
+	PTE* ptable=(PTE*)PTE_ADDR(pde.val);
+	PTE pte=(PTE)paddr_read((uint32_t)(ptable+PTX(addr)),4);
+	Assert(pte.present,"addr=0x%x",addr);
+
+	pde.accessed=1;
+	pte.accessed=1;
+	if(iswrite) pte.dirty=1;
+
+	paddr_t paddr=PTE_ADDR(pte.val)|OFF(addr);
+	return paddr;
+    }
+    return addr;
+}
+
 
 /* Memory accessing interfaces */
 
@@ -37,9 +71,27 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if(PTE_ADDR(addr)!=PTE_ADDR(addr+len-1))
+  {
+      printf("error:the data pass two pages\n");
+      assert(0);
+  }
+  else
+  {
+      paddr_t paddr=page_translate(addr,false);
+      return paddr_read(paddr, len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if(PTE_ADDR(addr)!=PTE_ADDR(addr+len+1))
+  {
+      printf("error:the data pass two page\n");
+      assert(0);
+  }
+  else
+  {
+      paddr_t paddr=page_translate(addr,true);
+      paddr_write(paddr, len, data);
+  }
 }
